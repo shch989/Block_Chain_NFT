fa1a378622654431f02075debee37b0d6038ac4f
shch989 <tankpop11@gmail.com>
2023-09-10 00:32:52 +0900

    if문 심화

--- Ch02/decision-making.sol
+++ Ch02/decision-making.sol
@@ -14,4 +14,19 @@ contract DecisionMaking {
         // 그렇지 않으면 false 반환
         return false;
     }
+
+    // 스택킹 지갑의 잔액을 나타내는 정수 변수 초기화
+    uint stackingWallet = 9;
+
+    // 에어드랍 함수: 스택킹 지갑의 값에 따라 결과 반환
+    function airDrop() public view returns (uint) {
+        // 만약 스택킹 지갑의 값이 10과 같으면
+        if (stackingWallet == 10) {
+            // 스택킹 지갑 값에 10을 더한 결과 반환
+            return stackingWallet + 10;
+        } else {
+            // 그렇지 않으면 스택킹 지갑 값에 1을 더한 결과 반환
+            return stackingWallet + 1;
+        }
+    }
 }
