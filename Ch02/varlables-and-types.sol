412ca22e7112eb0eb8321d1db91fea0c2c3e2069
shch989 <tankpop11@gmail.com>
2023-09-09 23:53:22 +0900

    변수 문제 풀이

--- Ch02/varlables-and-types.sol
+++ Ch02/varlables-and-types.sol
@@ -2,8 +2,24 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 contract learnVariables {
+    // 정수 변수 chocolateBar를 10으로 초기화합니다.
     uint chocolateBar = 10;
+
+    // 정수 변수 storeOwner를 300으로 초기화합니다.
     uint storeOwner = 300;
+
+    // 불린 변수 lieDetector를 true로 초기화합니다.
     bool lieDetector = true;
+
+    // 문자열 변수 errorMessageText를 "Error! There has been a mistake!"로 초기화합니다.
     string errorMessageText = "Error! There has been a mistake!";
+
+    // 정수 변수 wallet을 500으로 초기화합니다.
+    uint wallet = 500;
+
+    // 불린 변수 spend를 false로 초기화합니다.
+    bool spend = false;
+
+    // 문자열 변수 motifySpend를 "you have spent money"로 초기화합니다.
+    string motifySpend = "you have spent money";
 }
